4923430063d540a2643804f825beb255c25f8108
Alan Uraz <alanuraz@gmail.com>
2018-12-30 10:23:23 -0500

    Add title and date elements to page, increase image size, getelementbyid instead of by tag name

--- space.js
+++ space.js
@@ -1,19 +1,19 @@
 const spaceDiv = document.getElementsByClassName('space-pic-div')[0];
-let spaceImg;
-let textBox;
-
-for (let i = 0; i < spaceDiv.children.length; i++) {
-  const tag = spaceDiv.children[i];
-  if (tag.tagName === "IMG") spaceImg = tag;
-  if (tag.tagName === "P") textBox = tag;
-}
+const titleElement = document.getElementById('title');
+const dateElement = document.getElementById('date');
+const textElement = document.getElementById('text-box');
+const spaceImgElement = document.getElementById('space-img');
 
 const fetchImage = async () => {
   const response = await fetch('https://api.nasa.gov/planetary/apod?hd=true&api_key=m1ic6nHpL7Whqs593Vf1YlWwlCfi5FK6PflXdMdE');
   const jsonResp = await response.json();
+  const title = jsonResp.title;
+  const date = jsonResp.date;
   const explanation = jsonResp.explanation;
-  spaceImg.src = jsonResp.hdurl;
-  textBox.innerHTML = explanation;
+  spaceImgElement.src = jsonResp.hdurl;
+  textElement.innerHTML = explanation;
+  titleElement.innerHTML = title;
+  dateElement.innerHTML = date;
 };
 
 fetchImage();
