9382855db68148d7203a724f0149704ff3d32e81
Alan Uraz <alanuraz@gmail.com>
2019-01-05 09:46:03 -0500

    Break function into helpers, fumble with media queries

--- space.js
+++ space.js
@@ -5,15 +5,24 @@ const textElement = document.getElementById('text-box');
 const spaceImgElement = document.getElementById('space-img');
 
 const fetchImage = async () => {
-  const response = await fetch('https://api.nasa.gov/planetary/apod?hd=true&api_key=m1ic6nHpL7Whqs593Vf1YlWwlCfi5FK6PflXdMdE');
+  const response = await fetch('https://api.nasa.gov/planetary/apod?hd=false&api_key=m1ic6nHpL7Whqs593Vf1YlWwlCfi5FK6PflXdMdE');
   const jsonResp = await response.json();
-  const title = jsonResp.title;
+  return jsonResp;
+};
+
+const renderImage = (jsonResp) => {
   const date = jsonResp.date;
   const explanation = jsonResp.explanation;
-  spaceImgElement.src = jsonResp.hdurl;
+  const title = jsonResp.title;
+  spaceImgElement.src = jsonResp.url;
   textElement.innerHTML = explanation;
   titleElement.innerHTML = title;
   dateElement.innerHTML = date;
 };
 
-fetchImage();
+const createTheUniverse = async () => {
+  const jsonResp = await fetchImage();
+  renderImage(jsonResp);
+}
+
+createTheUniverse();
