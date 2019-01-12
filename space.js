58fffa67af3ba84bf8f3dbd5ffd54d2a7aa4dcf2
Alan Uraz <alanuraz@gmail.com>
2019-01-12 15:46:52 -0500

    Reduce image size, change colors, add HD photo configurable option

--- space.js
+++ space.js
@@ -3,9 +3,12 @@ const titleElement = document.getElementById('title');
 const dateElement = document.getElementById('date');
 const textElement = document.getElementById('text-box');
 const spaceImgElement = document.getElementById('space-img');
+const hd = true;
 
 const fetchImage = async () => {
-  const response = await fetch('https://api.nasa.gov/planetary/apod?hd=false&api_key=m1ic6nHpL7Whqs593Vf1YlWwlCfi5FK6PflXdMdE');
+  console.time('start')
+  const response = await fetch(`https://api.nasa.gov/planetary/apod?hd=${hd}&api_key=m1ic6nHpL7Whqs593Vf1YlWwlCfi5FK6PflXdMdE`);
+  console.timeEnd('start')
   const jsonResp = await response.json();
   return jsonResp;
 };
@@ -14,15 +17,18 @@ const renderImage = (jsonResp) => {
   const date = jsonResp.date;
   const explanation = jsonResp.explanation;
   const title = jsonResp.title;
-  spaceImgElement.src = jsonResp.url;
+  const urlType = hd ? 'hdurl' : 'url';
+  spaceImgElement.src = jsonResp[urlType];
   textElement.innerHTML = explanation;
   titleElement.innerHTML = title;
   dateElement.innerHTML = date;
 };
 
 const createTheUniverse = async () => {
+  console.time('universe')
   const jsonResp = await fetchImage();
   renderImage(jsonResp);
+  console.timeEnd('universe')
 }
 
 createTheUniverse();
